ab7a76396e52d30f4191ae81236f13e79fd42505
Tovarnov <avtovarnov@yandex.ru>
2024-02-29 11:50:40 +0300

    django forms

--- converter_proj/exchange/forms.py
+++ converter_proj/exchange/forms.py
@@ -2,5 +2,13 @@ from django import forms
 
 class ExchangeForm(forms.Form):
     inputAmount = forms.CharField(label='Введите сумму:', max_length=7)
-    from_curr = forms.ChoiceField(label='Из валюты')
+    from_curr = forms.ChoiceField(label='Из валюты', choices=[])
     to_curr = forms.ChoiceField(label='В валюту')
+
+    def __init__(self, *args, **kwargs):
+        context = kwargs.pop('data', None)
+        super(ExchangeForm, self).__init__(*args, **kwargs)
+        if context:
+            currencies = context.get('currencies', {})
+            self.fields['from_curr'].choices = [(currency, currency) for currency in currencies]
+            self.fields['to_curr'].choices = [(currency, currency) for currency in currencies]
--- converter_proj/exchange/views.py
+++ converter_proj/exchange/views.py
@@ -6,35 +6,40 @@ from exchange.forms import ExchangeForm
 
 
 def exchange(request):
-    # api_url = 'https://api.exchangerate-api.com/v4/latest/USD'
-    # response = requests.get(url=api_url).json()
+    api_url = 'https://api.exchangerate-api.com/v4/latest/USD'
+    response = requests.get(url=api_url).json()
 
-    # data = response.get('rates')
-    # if request.method == 'GET':
-    #     context = {
-    #         'currencies': data
-    #     }
+    data = response.get('rates')
+    if request.method == 'GET':
+        form = ExchangeForm(data = {'currencies': data})
+        context = {
+            'currencies': data,
+            'form': form
+        }
 
-    #     print(data)
-    #     return render(request, 'exchange/index.html', context=context)
+        print(data)
+        return render(request, 'exchange/index.html', context=context)
 
-    # if request.method == 'POST':
-    #     amount = float(request.POST.get('amount'))
-    #     from_curr = request.POST.get('from_curr')
-    #     to_curr = request.POST.get('to_curr')
+    if request.method == 'POST':
+        form = ExchangeForm(request.POST, data={'currencies': data})
+        if form.is_valid():
+            amount = float(form.cleaned_data['inputAmount'])
+            from_curr = form.cleaned_data['from_curr']
+            to_curr = form.cleaned_data['to_curr']
 
-    #     converted_amount = round((data[to_curr]/ data[from_curr]) * amount, 2)
+            converted_amount = round((data[to_curr] / data[from_curr]) * amount, 2)
 
-    #     context = {
-    #         'amount': amount,
-    #         'from_curr': from_curr,
-    #         'to_curr': to_curr,
-    #         'currencies': data,
-    #         'converted_amount': converted_amount
-    #     }
+            context = {
+                'amount': amount,
+                'from_curr': from_curr,
+                'to_curr': to_curr,
+                'currencies': data,
+                'converted_amount': converted_amount,
+                'form': form
+            }
 
-    #     print(context)
-    #     return render(request, 'exchange/index.html', context=context)
+            return render(request, 'exchange/index.html', context=context)
 
-    form = ExchangeForm()
-    return render(request, 'exchange/index.html', context={'form': form})
+    # data = {'currencies': {'USD': 1, 'AED': 3.67}}  # Пример списка валют
+    # form = ExchangeForm(context = data)
+    # return render(request, 'exchange/index.html', context={'form': form})
